8f8ca159047412a2bc5b751bd047fd842d61958b
Wilfred Hughes <me@wilfred.me.uk>
2013-04-04 14:46:50 +0100

    initial work on unit tests

---
+++ tests.el
@@ -0,0 +1,6 @@
+
+(fpr--git-root "/home/wilfred/personal/find-project-root.el/test_repos/git/features/initialization.feature")
+
+(fpr--svn-root "/home/wilfred/personal/find-project-root.el/test_repos/svn/Makefile")
+
+(fpr--cvs-root "/home/wilfred/personal/find-project-root.el/test_repos/cvs/emacs/tour/emacs.css")
