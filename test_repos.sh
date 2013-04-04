69e86baf786076b1680845d302cfef1299a82780
Wilfred Hughes <me@wilfred.me.uk>
2013-04-04 14:46:43 +0100

    adding a test CVS repo the test_repos script

--- test_repos.sh
+++ test_repos.sh
@@ -13,3 +13,10 @@ if [ ! -d ./test_repos/svn ];
 then
     svn checkout http://js2-mode.googlecode.com/svn/trunk/ test_repos/svn
 fi
+
+if [ ! -d ./test_repos/cvs ];
+then
+    mkdir test_repos/cvs
+    cd test_repos/cvs
+    cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/web/emacs co emacs/tour
+fi
