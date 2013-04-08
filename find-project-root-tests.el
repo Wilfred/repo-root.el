a00f8988bdd30db313da1e4539f519691380060a
Wilfred Hughes <me@wilfred.me.uk>
2013-04-08 12:18:15 +0100

    adding a proper ert test and convenience test running command

--- find-project-root-tests.el
+++ find-project-root-tests.el
@@ -1,5 +1,16 @@
+(require 'ert)
 
-(fpr--git-root "/home/wilfred/personal/find-project-root.el/test_repos/git/features/initialization.feature")
+(ert-deftest fpr-test-git ()
+    (let* ((git-repo-root "/home/wilfred/personal/find-project-root.el/test_repos/git")
+           (git-file-path (concat (file-name-as-directory git-repo-root) "features/initialization.feature")))
+      (should
+       (equal
+        git-repo-root
+        (find-project-root git-file-path)))))
+
+(defun run-fpr-tests ()
+  (interactive)
+  (ert-run-tests-interactively "fpr-test-"))
 
 (fpr--svn-root "/home/wilfred/personal/find-project-root.el/test_repos/svn/Makefile")
 
